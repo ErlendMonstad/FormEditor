d020e584f5fdc48b2db1c2f431e888fd844434d7
Erlend Monstad <erlendmonstad@gmail.com>
2020-03-16 18:15:42 +0100

    Grid blir i større grad lagd av Javascript.

--- formeditor.js
+++ formeditor.js
@@ -11,6 +11,12 @@ function drop(ev) {
     ev.preventDefault();
     let data = ev.dataTransfer.getData("text");
     let el = document.getElementById(data);
+    if(el.classList.contains("cell")){
+        let x = el.getAttribute("data-x");
+        let y = el.getAttribute("data-y");
+
+    }
+
     if(el.classList.contains("tool")){
         let newelement = el.cloneNode(true);
         //newelement.classList.add("copy");
@@ -18,7 +24,26 @@ function drop(ev) {
         newelement.classList.remove("tool");
         ev.target.appendChild(newelement);
     }else{
-        ev.target.appendChild(document.getElementById(data));
+
+        document.getElementById("grid").appendChild(document.getElementById(data));
+    }
+
+
+}
+
+
+function populateGrid(){
+    let div = document.createElement('div');
+    div.outerHTML = '<div class="cell" ondrop="drop(event)" ondragover="allowDrop(event)" />';
+    for(let i = 0; i < 12 * 20; i++){
+        let cell = div.cloneNode(true);
+        let x = document.createAttribute("data-x");
+        let y = document.createAttribute("data-y");
+        x.value = i % 12;
+        y.value = (i - x.value) / 12;
+        cell.attributes.setNamedItem(x);
+        cell.attributes.setNamedItem(y);
+        document.getElementById("grid").appendChild(cell);
     }
 
 
@@ -29,3 +54,5 @@ let idcounter = 0;
 function newId(){
     return "id" + idcounter++;
 }
+
+populateGrid();
