5a11e569c5a66fa967055f907108de28d57e85e3
Erlend Monstad <erlendmonstad@gmail.com>
2020-03-23 22:57:08 +0100

    Labels er lagt inn i en div så det blir enklere å håndtere.

--- gridelement.js
+++ gridelement.js
@@ -14,7 +14,7 @@ Vue.component('grid-element', {
         }
     },
 
-    template:   '<p v-if=\'item.type === "label"\' :style="gridarea" :ondragstart="ondragstart" :onmousemove="onmousemove">{{item.value}}</p>' +
+    template:   '<div v-if=\'item.type === "label"\' class="cell" :style="gridarea" :ondragstart="ondragstart" :onmousemove="onmousemove"><p>{{item.value}}</p></div>' +
         '<input v-else-if=\'item.type === "text"\' :value="item.value" type="text" :style="gridarea":ondragstart="ondragstart" :onmousemove="onmousemove"/>' +
         '<input v-else-if=\'item.type === "button"\' :value="item.value" type="button" :style="gridarea" :ondragstart="ondragstart" :onmousemove="onmousemove"/>'
 });
