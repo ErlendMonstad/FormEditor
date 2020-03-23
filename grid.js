04d268d3b5aa52f7db5cf0c76a5aa8d987d101fc
Erlend Monstad <erlendmonstad@gmail.com>
2020-03-23 22:13:54 +0100

    Resize funksjonalitet fikset.

--- grid.js
+++ grid.js
@@ -19,23 +19,21 @@ function pointerDirection(item,margin, pageX,pageY){
     let columnwidth = (domrect.width / coloums);
     let rowheight = (domrect.height / rows);
 
-    let horDirection = (gridx - margin <= item.x * columnwidth) ? "e" : ( gridx + margin >= (item.x + item.w)  * columnwidth) ? "w" : "";
-    let verDirection = (gridy - margin <= item.y * rowheight) ? "s" : ( gridy + margin >= (item.y + item.h)  * rowheight) ? "n" : "";
+    let horDirection = (gridx - margin <= item.x * columnwidth) ? "w" : ( gridx + margin >= (item.x + item.w)  * columnwidth) ? "e" : "";
+    let verDirection = (gridy - margin <= item.y * rowheight) ? "n" : ( gridy + margin >= (item.y + item.h)  * rowheight) ? "s" : "";
     let direction =  verDirection + horDirection;
-    return direction;
-
+    return {verDirection: verDirection, horDirection: horDirection, direction : direction };
 }
 
 function setPointer(event,id){
     let item = app.gridlist.find(item => item.id == id);
     let pd = pointerDirection(item,5,event.pageX,event.pageY);
 
-    console.log(pd);
     // Temporary
-    if(pd === ""){
+    if(pd.direction === ""){
         event.target.style.cursor = "default"
     }else{
-        let pointerType = pd + "-resize";
+        let pointerType = pd.direction + "-resize";
         event.target.style.cursor = pointerType;
     }
 
@@ -55,15 +53,14 @@ function drag(event,id) {
     let columnwidth = (domrect.width / coloums);
     let rowheight = (domrect.height / rows);
 
-    let x = Math.floor((event.pageX - domrect.x) / columnwidth) - item.x;
-    let y = Math.floor((event.pageY - domrect.y) / rowheight) - item.y;
+    let x = Math.floor(gridx / columnwidth) - item.x;
+    let y = Math.floor(gridy/ rowheight) - item.y;
 
-    let islastcolumn = x >= item.w - 1;
-    let islastrow = y >= item.h - 1;
+    let pD = pointerDirection(item,margin,event.pageX,event.pageY);
+    let mode = (pD.direction === "") ? "move" : "resize";
 
-    let mode = (pointerDirection(item,margin,event.pageX,event.pageY) === "") ? "move" : "resize";
 
-    event.dataTransfer.setData("text", JSON.stringify({id:id, x: x, y: y, mode: mode, islastrow:islastrow, islastcolumn: islastcolumn }));
+    event.dataTransfer.setData("text", JSON.stringify({id:id, x: x, y: y, mode: mode, verDirection: pD.verDirection, horDirection:pD.horDirection }));
     event.dataTransfer.dropEffect = "none";
     //ev.dataTransfer.setData("text",ev.target.className)
 }
@@ -90,28 +87,27 @@ function drop(event) {
     let element_x = data.x;
     let element_y = data.y;
 
-    console.log(data.mode);
-    if(data.mode === "resize"){
-        if(data.islastcolumn){
-            if(grid_x < element_x.x){
-                item.x = grid_x - data.x + item.w;
-                item.w = item.w - (grid_x - item.x);
-            }else{
-                item.w = grid_x - item.x;
-            }
-
-        }
-        if(data.islastrow){
-            if(grid_y < element_y){
-                item.y = grid_y + item.h;
-                item.h = item.h - (grid_y - item.y);
-            }else{
-                item.h = grid_y - item.y;
-            }
-        }
-    }else {
+    if(data.mode === "move"){
         item.x = grid_x - data.x;
         item.y = grid_y - data.y;
+    }else if(data.mode === "resize"){
+        if(data.verDirection === "n"){
+            item.h += (data.y + item.y) - grid_y;
+            item.y = grid_y;
+
+        }else if(data.verDirection === "s"){
+            item.h = grid_y - item.y + 1;
+        }
+        if(data.horDirection === "w"){
+            item.w += (data.x + item.x) - grid_x;
+            item.x = grid_x;
+
+        }else if(data.horDirection === "e"){
+            item.w = grid_x - item.x + 1;
+        }
+        // Ensure that width and height isn't negative. Might be able to be moved to the component. 
+        item.w = Math.max(1,item.w);
+        item.h = Math.max(1, item.h);
     }
 
 }
