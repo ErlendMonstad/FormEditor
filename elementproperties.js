060e3974b4390038f6cad2de1664b3371d0e738f
Erlend Monstad <erlendmonstad@gmail.com>
2020-03-24 19:48:44 +0100

    Fikset CSS på property menyen.

--- elementproperties.js
+++ elementproperties.js
@@ -9,7 +9,7 @@ Vue.component('elementproperties', {
         }
     },
     template:
-        '<div id="property"><h2>Element Properties</h2>' +
+        '<div class="property">' +
         '<p>{{item.value}}</p>' +
         '<label for="newValue">Value:</label>' +
         '<input type="text" id="newValue" @keyup.enter="updateValue" v-model="value">' +
