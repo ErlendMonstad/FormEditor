4496eea1a6436f70d7dc7c6c905c99fbb01d49ad
h182033 <joakim_hogelid@hotmail.com>
2020-03-24 01:29:38 +0100

    property meny begynt

---
+++ elementproperties.js
@@ -0,0 +1,17 @@
+Vue.component('elementproperties', {
+    props: ['item', 'value'],
+    data: {
+        value: ''
+    },
+    methods: {
+        updateValue() {
+            updateElement(event, this.item.id, this.value)
+        }
+    },
+    template:
+        '<div><h2>Element Properties</h2>' +
+        '<p>{{item.value}}</p>' +
+        '<label for="newValue">Value:</label>' +
+        '<input type="text" id="newValue" @keyup.enter="updateValue" v-model="value">' +
+        '</div>'
+});
