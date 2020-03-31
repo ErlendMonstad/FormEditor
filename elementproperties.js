aa750a9e84b4ec2b7484a2f56c72f1ecb990b706
h182033 <21162137+h182033@users.noreply.github.com>
2020-03-31 22:49:28 +0200

    property

--- elementproperties.js
+++ elementproperties.js
@@ -1,16 +1,18 @@
 Vue.component('elementproperties', {
-    props: ['item', 'value'],
-    data: {
-        value: ''
+    props: ['item'],
+    data: function() {
+        return{
+            message: this.item.value
+        }
     },
     methods: {
         updateValue() {
-            updateElement(event, this.item.id, this.value)
+            updateElement(event, this.item.id, this.message)
         }
     },
     template:
         '<div class="property">' +
-        '<label for="newValue">{{item.id}} {{item.value}}</label>' +
-        '<input class="newValue" type="text" id="newValue" @keyup="updateValue" v-model="value">' +
+        '<label for="newValue">{{item.id}} {{message}}</label>' +
+        '<input class="newValue" type="text" id="newValue" @keyup="updateValue" v-model="message">' +
         '</div>'
 });
