fbc1065d8c0ed6cd06ba21277169c5ccdd8a1f95
Erlend Monstad <erlendmonstad@gmail.com>
2020-04-06 20:21:59 +0200

    Fjernet Bootstrap CSS, og IDer i propertymenyen.

--- js/elementproperties.js
+++ js/elementproperties.js
@@ -14,7 +14,7 @@ Vue.component('elementproperties', {
     },
     template:
         '<div class="property">' +
-        '<label for="newValue">{{item.id}} {{item.value}}</label>' +
+        '<label for="newValue">{{item.value}}</label>' +
         '<input class="newValue" type="text" id="newValue" v-model="item.value">' +
         '</div>'
 });
