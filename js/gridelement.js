03fe0b496cfb06fa369e90df0ccdbe72708ddea0
h182033 <21162137+h182033@users.noreply.github.com>
2020-04-07 16:38:19 +0200

    delete elements

--- js/gridelement.js
+++ js/gridelement.js
@@ -14,12 +14,12 @@ Vue.component('grid-element', {
         }
     },
 
-    template:   '<div v-if=\'item.type === "label"\' class="cell border" :style="gridarea" :ondragstart="ondragstart" :onmousemove="onmousemove"><p>{{item.value}}</p></div>' +
-        '<div v-else-if=\'item.type === "textbox"\' class="cell" :style="gridarea":ondragstart="ondragstart" :onmousemove="onmousemove" ><input :value="item.value" type="text" /></div>' +
-        '<div v-else-if=\'item.type === "button"\' :style="gridarea" :ondragstart="ondragstart" :onmousemove="onmousemove"><input :value="item.value" type="button" /></div>' +
-        '<div v-else-if=\'item.type === "radio-button"\' class="cell border radio" :style="gridarea" :ondragstart="ondragstart" :onmousemove="onmousemove"><radio-element :value="item.value"></radio-element></div>' +
-        '<div v-else-if=\'item.type === "dropdown"\' class="cell border dropdown" :style="gridarea" :ondragstart="ondragstart" :onmousemove="onmousemove"><dropdown-element :value="item.value"></dropdown-element></div>' +
-        '<div v-else-if=\'item.type === "checkbox"\' class="cell border checkbox" :style="gridarea" :ondragstart="ondragstart" :onmousemove="onmousemove"><checkbox-element :value="item.value"></checkbox-element></div>' +
-        '<div v-else-if=\'item.type === "image"\' class="cell border checkbox" :style="gridarea" :ondragstart="ondragstart" :onmousemove="onmousemove"><image-element :value="item.value"></image-element></div>' +
-        '<div v-else-if=\'item.type === "headline"\' class="cell border" :style="gridarea" :ondragstart="ondragstart" :onmousemove="onmousemove"><h1>{{item.value}}</h1></div>'
+    template:   '<div v-if=\'item.type === "label"\' class="cell border" :style="gridarea" :ondragstart="ondragstart" :onmousemove="onmousemove"><p>{{item.value}}<delete-element v-bind:item="this.item"></delete-element></p></div>' +
+        '<div v-else-if=\'item.type === "textbox"\' class="cell" :style="gridarea":ondragstart="ondragstart" :onmousemove="onmousemove" ><input :value="item.value" type="text" /><delete-element v-bind:item="this.item"></delete-element></div>' +
+        '<div v-else-if=\'item.type === "button"\' :style="gridarea" :ondragstart="ondragstart" :onmousemove="onmousemove"><input :value="item.value" type="button" /><delete-element v-bind:item="this.item"></delete-element></div>' +
+        '<div v-else-if=\'item.type === "radio-button"\' class="cell border radio" :style="gridarea" :ondragstart="ondragstart" :onmousemove="onmousemove"><radio-element :value="item.value"></radio-element><delete-element v-bind:item="this.item"></delete-element></div>' +
+        '<div v-else-if=\'item.type === "dropdown"\' class="cell border dropdown" :style="gridarea" :ondragstart="ondragstart" :onmousemove="onmousemove"><dropdown-element :value="item.value"><delete-element></delete-element v-bind:item="this.item"></dropdown-element></div>' +
+        '<div v-else-if=\'item.type === "checkbox"\' class="cell border checkbox" :style="gridarea" :ondragstart="ondragstart" :onmousemove="onmousemove"><checkbox-element :value="item.value"></checkbox-element><delete-element v-bind:item="this.item"></delete-element></div>' +
+        '<div v-else-if=\'item.type === "image"\' class="cell border checkbox" :style="gridarea" :ondragstart="ondragstart" :onmousemove="onmousemove"><image-element :value="item.value"><delete-element v-bind:item="this.item"></delete-element></image-element></div>' +
+        '<div v-else-if=\'item.type === "headline"\' class="cell border" :style="gridarea" :ondragstart="ondragstart" :onmousemove="onmousemove"><h1>{{item.value}}<delete-element v-bind:item="this.item"></delete-element></h1></div>'
 });
